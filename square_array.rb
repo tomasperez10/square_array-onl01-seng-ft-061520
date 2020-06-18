e19bb6a7ccf98f4141da99231c3ab4de44725647
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 00:54:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each { | numbers |
-    new_array.push(numbers ** 2)
+  array.each { | numbers | new_array.push(numbers ** 2) }
   return new_array
 end
 
